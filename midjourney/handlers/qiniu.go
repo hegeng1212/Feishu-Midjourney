8b5e993ee49927f1a50ab0f28b168893ca64c9cc
hegeng <hegeng@babytree-inc.com>
2023-06-15 19:41:21 +0800

    fix

--- midjourney/handlers/qiniu.go
+++ midjourney/handlers/qiniu.go
@@ -60,8 +60,6 @@ func QiniuUploadImage(attachments []*discord.MessageAttachment) (newAttachments 
 		newAttachments = append(newAttachments, newAttachment)
 	}
 
-	fmt.Println(fmt.Printf("newAttachments %#v", newAttachments))
-
 	return
 }
 
@@ -89,7 +87,6 @@ func UploadImage(localFile string, key string) (ret *storage.PutRet, err error) 
 	formUploader := storage.NewFormUploader(&cfg)
 	ret = &storage.PutRet{}
 	putExtra := storage.PutExtra{}
-	fmt.Println(fmt.Printf("upToken %s localFile %s key %s", upToken, localFile, key))
 	err = formUploader.PutFile(context.Background(), &ret, upToken, key, localFile, &putExtra)
 	if err != nil {
 		return
