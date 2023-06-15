50ee0d3f0b1efdb481af35174fb7053e4259a373
hegeng <hegeng@babytree-inc.com>
2023-06-15 19:38:07 +0800

    fix

--- midjourney/main.go
+++ midjourney/main.go
@@ -17,7 +17,8 @@ func main() {
 	initialization.LoadConfig(*cfg)
 	initialization.LoadDiscordClient(handlers.DiscordMsgCreate, handlers.DiscordMsgUpdate)
 
-	ret, err := handlers.UploadImage("/tmp/tangcheng_futuristic_city_c8cf7f74691fb7543f2baa1055edf889_149bd28d-f5c1-492c-89dc-f58d1245a083.png", "/attachments/1112690540476104734/1118861394272583820/tangcheng_futuristic_city_c8cf7f74691fb7543f2baa1055edf889_149bd28d-f5c1-492c-89dc-f58d1245a083.png")
+	key := strings.TrimLeft("/attachments/1112690540476104734/1118861394272583820/tangcheng_futuristic_city_c8cf7f74691fb7543f2baa1055edf889_149bd28d-f5c1-492c-89dc-f58d1245a083.png", "/")
+	ret, err := handlers.UploadImage("/tmp/tangcheng_futuristic_city_c8cf7f74691fb7543f2baa1055edf889_149bd28d-f5c1-492c-89dc-f58d1245a083.png", key)
 	fmt.Println(fmt.Printf("%#v %#v", ret, err))
 
 	r := gin.Default()
